9cd04841db540327acfd1749c52a7d899246bb9c
Lototskiy <34381097+Lototskiy@users.noreply.github.com>
2017-12-21 23:40:51 +0200

    slider

--- js/common.js
+++ js/common.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-	
+	$('.slickslider').slick({
+		autoplay: true,
+		autoplaySpeed: 1000,
+		cssEase:'ease',
+		dots: true,
+		arrows: false,
+		pauseOnDotsHover: true,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+		slidesToScroll: 1,
+      }
+    }] 
+	})
 });
