9dc9e605d3153cee6d51b0dae1eea1f954a4bf2d
dima lototskiy <dmytro.lototskiy@gmail.com>
2020-04-02 13:31:43 +0300

    js

--- Trucking/js/main.js
+++ Trucking/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function(){
   $('.slider').slick({
     slidesToScroll: 1,
@@ -15,21 +14,3 @@ $(function(){
      $('.header__btn').toggleClass('active');
    });
 });
-=======
-$(function(){
-  $('.slider').slick({
-    slidesToScroll: 1,
-    dots: false,
-    arrows: true,
-    prevArrow: $('.arrow-left'),
-    nextArrow: $('.arrow-right')
-  });
-  //WOW.JS
-  new WOW().init();
-
-   $('.header__btn').on('click', function () {
-     $('.header__nav').slideToggle();
-     $('.header__btn').toggleClass('active');
-   });
-});
->>>>>>> 4c002262aad30a5a83ad9103f31d641a9d78eb20
