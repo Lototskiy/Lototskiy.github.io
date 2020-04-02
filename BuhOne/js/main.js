bfe5ba74c93d42d1262c22a2ddb8459c8522eba6
dima lototskiy <dmytro.lototskiy@gmail.com>
2020-04-02 13:21:27 +0300

    js

--- BuhOne/js/main.js
+++ BuhOne/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function(){
   $('.slider').slick({
     slidesToScroll: 1,
@@ -27,34 +26,3 @@ $(function(){
     $('.menu__nav').toggleClass('nav__active');
     $('.menu__contact').toggleClass('contact__active');
   });
-=======
-$(function(){
-  $('.slider').slick({
-    slidesToScroll: 1,
-    dots: true,
-    arrows: true,
-    fade: true,
-    prevArrow: $('.hero__prev-arrow'),
-    nextArrow: $('.hero__next-arrow')
-  });
-
-  $('.sliderClient').slick({
-    dots: true,
-    arrows: true,
-    prevArrow: $('.client__prev-arrow'),
-    nextArrow: $('.client__next-arrow')
-  });
-  $('.sliderReviews').slick({
-    dots: true,
-    arrows: true,
-    prevArrow: $('.reviews__prev-arrow'),
-    nextArrow: $('.reviews__next-arrow')
-  });
-
-  $('.menu__btn').on('click', function () {
-    $('.menu__btn').toggleClass('active__btn');
-    $('.menu__nav').toggleClass('nav__active');
-    $('.menu__contact').toggleClass('contact__active');
-  });
->>>>>>> 4c002262aad30a5a83ad9103f31d641a9d78eb20
-});
