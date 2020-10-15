58bf71294f0c0991e2935118eb965e0db7b528d2
Dima <Dmytro.lototskiy@gmail>
2020-10-15 16:32:18 +0300

    finish

---
+++ heatSupply/src/index.js
@@ -0,0 +1,35 @@
+import './assets/css/normalize.scss';
+import './assets/css/main.scss';
+
+
+//BLOCKS 
+import './assets/css/blocks/header.scss';
+import './assets/css/blocks/footer.scss';
+import './assets/css/blocks/main-screen.scss';
+import './assets/css/blocks/btn.scss';
+import './assets/css/blocks/link.scss';
+import './assets/css/blocks/about.scss';
+import './assets/css/blocks/faq.scss';
+import './assets/css/blocks/information.scss';
+import './assets/css/blocks/contact.scss';
+import './assets/css/blocks/request.scss';
+import './assets/css/blocks/inputs.scss';
+import './assets/css/blocks/application-form.scss';
+import './assets/css/blocks/reviews.scss';
+import './assets/css/blocks/services.scss';
+import './assets/css/blocks/grid.scss';
+import './assets/css/blocks/maintenance.scss';
+import './assets/css/blocks/builders.scss';
+import './assets/css/blocks/mobile-menu.scss';
+import './assets/css/blocks/popup.scss';
+import './assets/css/blocks/popup-form.scss';
+import './assets/css/blocks/reports.scss';
+
+
+
+
+import './assets/js/anchors';
+import './assets/js/faq';
+import './assets/js/fixed-menu';
+import './assets/js/mobile-menu';
+import './assets/js/popup';
